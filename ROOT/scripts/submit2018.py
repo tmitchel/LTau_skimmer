f291eb64e74f81c8d921705ba9e6d4ab2bef9332
Tyler Mitchell <tyler106@ksu.edu>
2019-08-20 10:48:38 -0500

    add new samples

--- ROOT/scripts/submit2018.py
+++ ROOT/scripts/submit2018.py
@@ -11,14 +11,6 @@ parser.add_argument('-l', '--lepton', action='store',
                     help='which lepton (mt or et)')
 args = parser.parse_args()
 
-test_batch = {
-    'DYJets1': ['DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v1', 'Z'],
-}
-
-temp = {
-    'WJets': ['WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v2', 'W'],
-}
-
 bkg_samples_batch1 = {
     'DYJets1_v1': ['DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v1', 'Z'],
     'DYJets1_v2': ['DY1JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v2', 'Z'],
@@ -38,8 +30,7 @@ bkg_samples_batch1 = {
     'WJets4_v1': ['W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v1', 'W'],
     'WJets4_v2': ['W4JetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v2', 'W'],
     'WJets': ['WJetsToLNu_TuneCP5_13TeV-madgraphMLM-pythia8_-102X_upgrade2018_realistic_v15-v2', 'W'],
-}
-bkg_samples_batch2 = {
+
     'EWKWMinus': ['EWKWMinus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'EWKWPlus': ['EWKWPlus2Jets_WToLNu_M-50_TuneCP5_13TeV-madgraph-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'EWKZ2l': ['EWKZ2Jets_ZToLL_M-50_TuneCP5_PSweights_13TeV-madgraph-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
@@ -48,61 +39,63 @@ bkg_samples_batch2 = {
     'T-tchan': ['ST_t-channel_top_5f_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'Tbar-tW': ['ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15_ext1-v1', '0'],
     'T-tW': ['ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15_ext1-v1', '0'],
-    'TTLep': ['TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
-    'TTHad': ['TTToHadronic_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
-    'TTSemi': ['TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'WW_v1': ['WW_TuneCP5_13TeV-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'WW_v2': ['WW_TuneCP5_13TeV-pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
     'WZ_v1': ['WZ_TuneCP5_13TeV-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
     'WZ_v2': ['WZ_TuneCP5_13TeV-pythia8_-102X_upgrade2018_realistic_v15-v3', '0'],
     'ZZ': ['ZZ_TuneCP5_13TeV-pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
 }
-
-sig_samples = {
-    'ggh125_powheg': ['GluGluHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', 'Z'],
-    'vbf125_powheg': ['VBFHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15_ext1-v1', 'Z'],
-    'wminus125_powheg': ['WminusHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
-    'wplus125_powheg': ['WplusHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
-    'zh125_powheg': ['ZHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
+bkg_samples_batch2 = {
+    'TTLep': ['TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
+    'TTHad': ['TTToHadronic_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
+    'TTSemi': ['TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8_-102X_upgrade2018_realistic_v15-v1', '0'],
 }
 
-# Needs to be updated
-ac_samples = {
-    'ggH_a1': ['JJHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'ggH_a3': ['JJHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'ggH_a3int': ['JJHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_a1': ['VBFHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_a2': ['VBFHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_a2int': ['VBFHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_a3': ['VBFHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_a3int': ['VBFHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_l1': ['VBFHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_l1int': ['VBFHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_l1zg': ['VBFHiggs0L1ZgToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'vbf_l1zgint': ['VBFHiggs0L1Zgf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
-    'wh_a1': ['WHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'wh_a2': ['WHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'wh_a2int': ['WHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v3', '0'],
-    'wh_a3': ['WHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v3', '0'],
-    'wh_a3int': ['WHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'wh_l1': ['WHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'wh_l1int': ['WHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_a1': ['ZHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_a2': ['ZHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_a2int': ['ZHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_a3': ['ZHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_a3int': ['ZHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_l1': ['ZHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_l1int': ['ZHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-    'zh_l1zgint': ['ZHiggs0L1Zgf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
-}
+# sig_samples = {
+#     'ggh125_powheg': ['GluGluHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', 'Z'],
+#     'vbf125_powheg': ['VBFHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15_ext1-v1', 'Z'],
+#     'wminus125_powheg': ['WminusHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
+#     'wplus125_powheg': ['WplusHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
+#     'zh125_powheg': ['ZHToTauTau_M125_13TeV_powheg_pythia8_-102X_upgrade2018_realistic_v15-v2', '0'],
+# }
 
-# Needs to be updated
-madgraph_samples = {
-    "ggH_TwoJet_madgraph": ["GluGluToHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
-    "ggH_PS_TwoJet_madgraph": ["GluGluToPseudoscalarHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
-    "ggH_Maxmix_TwoJet_madgraph": ["GluGluToMaxmixHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
-}
+# # Needs to be updated
+# ac_samples = {
+#     'ggH_a1': ['JJHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'ggH_a3': ['JJHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'ggH_a3int': ['JJHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_a1': ['VBFHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_a2': ['VBFHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_a2int': ['VBFHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_a3': ['VBFHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_a3int': ['VBFHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_l1': ['VBFHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_l1int': ['VBFHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_l1zg': ['VBFHiggs0L1ZgToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'vbf_l1zgint': ['VBFHiggs0L1Zgf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', 'Z'],
+#     'wh_a1': ['WHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'wh_a2': ['WHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'wh_a2int': ['WHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v3', '0'],
+#     'wh_a3': ['WHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v3', '0'],
+#     'wh_a3int': ['WHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'wh_l1': ['WHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'wh_l1int': ['WHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_a1': ['ZHiggs0PMToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_a2': ['ZHiggs0PHToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_a2int': ['ZHiggs0PHf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_a3': ['ZHiggs0MToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_a3int': ['ZHiggs0Mf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_l1': ['ZHiggs0L1ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_l1int': ['ZHiggs0L1f05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+#     'zh_l1zgint': ['ZHiggs0L1Zgf05ph0ToTauTau_M125_13TeV_JHUGenV7011_pythia8__94X_mc2017_realistic_v14-v2', '0'],
+# }
+
+# # Needs to be updated
+# madgraph_samples = {
+#     "ggH_TwoJet_madgraph": ["GluGluToHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
+#     "ggH_PS_TwoJet_madgraph": ["GluGluToPseudoscalarHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
+#     "ggH_Maxmix_TwoJet_madgraph": ["GluGluToMaxmixHToTauTauPlusTwoJets_M125_13TeV_amcatnloFXFX_pythia8__94X_mc2017_realistic_v14-v1", 'Z'],
+# }
 
 el_data_samples = {
     'datasE-A': ['data_EGamma_Run2018A-17Sep2018', '0'],
@@ -120,43 +113,42 @@ mu_data_samples = {
 
 # Need to be updated
 el_embedded_samples = {
-    'embedEl-A': ['embedded_EmbeddingRun2017B_ElTauFinalState', '0'],
-    'embedEl-B': ['embedded_EmbeddingRun2017B_ElTauFinalState', '0'],
-    'embedEl-C': ['embedded_EmbeddingRun2017C_ElTauFinalState', '0'],
-    'embedEl-D': ['embedded_EmbeddingRun2017D_ElTauFinalState', '0'],
+    'embedEl-A': ['embedded_EmbeddingRun2018A_ElTauFinalState', '0'],
+    'embedEl-B': ['embedded_EmbeddingRun2018B_ElTauFinalState', '0'],
+    'embedEl-C': ['embedded_EmbeddingRun2018C_ElTauFinalState', '0'],
+    'embedEl-D': ['embedded_EmbeddingRun2018D_ElTauFinalState', '0'],
 }
 
 # Need to be updated
 mu_embedded_samples = {
-    'embedMu-A': ['embedded_EmbeddingRun2017B_MuTauFinalState', '0'],
-    'embedMu-B': ['embedded_EmbeddingRun2017B_MuTauFinalState', '0'],
-    'embedMu-C': ['embedded_EmbeddingRun2017C_MuTauFinalState', '0'],
-    'embedMu-D': ['embedded_EmbeddingRun2017D_MuTauFinalState', '0'],
+    'embedMu-A': ['embedded_EmbeddingRun2018A_MuTauFinalState', '0'],
+    'embedMu-B': ['embedded_EmbeddingRun2018B_MuTauFinalState', '0'],
+    'embedMu-C': ['embedded_EmbeddingRun2018C_MuTauFinalState', '0'],
+    'embedMu-D': ['embedded_EmbeddingRun2018D_MuTauFinalState', '0'],
 }
 
 prefix = args.prefix
 jobType = args.job
 
-bkg_pref = '/hdfs/store/user/caillol/SMHTT_legacy_2018_240419/'
-sig_pref = bkg_pref
-data_pref = '/hdfs/store/user/caillol/SMHTT_legacy_2018_data_24042019/'
+bkg_pref = '/hdfs/store/user/caillol/SMHTT_2018_legacy_MC_17jul/'
+bkg2_pref = '/hdfs/store/user/caillol/SMHTT_2018_legacy_MC_25jul/'
+data_pref = '/hdfs/store/user/caillol/SMHTT_2018_data/'
+embed_pref = '/hdfs/store/user/caillol/SMHTT_2018_embedded/'
 # Need updated
-ac_pref = '/hdfs/store/user/ymaravin/ac2017_v2/'
-madgraph_pref = '/hdfs/store/user/ymaravin/mg2017_v1/'
-embed_pref = '/hdfs/store/user/caillol/SMHTT2017_embedded_8nov/'
+# sig_pref = bkg_pref
+# ac_pref = '/hdfs/store/user/ymaravin/ac2017_v2/'
+# madgraph_pref = '/hdfs/store/user/ymaravin/mg2017_v1/'
 
 settings = {
-    'sig': [sig_pref, sig_samples],
-    'ac': [ac_pref, ac_samples],
-    'madgraph': [madgraph_pref, madgraph_samples],
+    # 'sig': [sig_pref, sig_samples],
+    # 'ac': [ac_pref, ac_samples],
+    # 'madgraph': [madgraph_pref, madgraph_samples],
     'dataMu': [data_pref, mu_data_samples],
     'dataEl': [data_pref, el_data_samples],
     'embedMu': [embed_pref, mu_embedded_samples],
     'embedEl': [embed_pref, el_embedded_samples],
     'bkg1': [bkg_pref, bkg_samples_batch1],
-    'bkg2': [bkg_pref, bkg_samples_batch2],
-    'test': [bkg_pref, test_batch],
-    'temp': [bkg_pref, temp]
+    'bkg2': [bkg2_pref, bkg_samples_batch2],
 }
 
 pref = settings[args.job][0]
